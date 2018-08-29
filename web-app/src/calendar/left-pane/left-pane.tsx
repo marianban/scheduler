dfc3b5ba8ce8f56714da71793257af6e38aaa710
Marian Ban <marian.ban@gmail.com>
2018-08-29 20:23:44 +0200

    calendar

--- web-app/src/calendar/left-pane/left-pane.tsx
+++ web-app/src/calendar/left-pane/left-pane.tsx
@@ -1,13 +1,49 @@
-import { getDaysInMonth } from 'date-fns';
+import * as classNames from 'classnames';
+import { addDays, getDay, getDaysInMonth, getISOWeek } from 'date-fns';
 import * as React from 'react';
+import './calendar.css';
 
-const range = (count: number) => Array.from({ length: count }, (v, k) => k + 1);
+const range = (count: number, start: number = 1) =>
+  Array.from({ length: count }, (v, i) => start + i);
 
 const LeftPane = () => {
+  const date = new Date();
+  const firstDayInMonth = new Date(date.getFullYear(), date.getMonth());
+  const firstWeekInMonth = getISOWeek(firstDayInMonth);
+  const numberOfWeeks = 5;
   return (
-    <aside className="app__left-pane" >
-      August 2018
-      {range(getDaysInMonth(new Date())).map((day: number) => day)}
+    <aside className="app__left-pane">
+      <div className="calendar">
+        <div className="calendar__month">
+          <div>&lt;</div>
+          <div className="calendar__month__title">August 2018</div>
+          <div>&gt;</div>
+        </div>
+        <div className="calendar__weeks">
+          <div className="calendar__week" />
+          {range(numberOfWeeks, firstWeekInMonth).map((week: number) => (
+            <div className="calendar__week" key={week}>
+              {week}
+            </div>
+          ))}
+        </div>
+        <div className="calendar__days">
+          {['Mo', 'Tu', 'We', 'Th', 'Fi', 'Sa', 'Su'].map((dayName: string) => (
+            <div className="calendar__day__name">{dayName}</div>
+          ))}
+          {range(getDaysInMonth(firstDayInMonth)).map((day: number) => (
+            <div
+              className={classNames('calendar__day', {
+                'calendar__day--today': date.getDate() === day
+              })}
+              key={day}
+              style={{ gridColumn: getDay(addDays(firstDayInMonth, day - 1)) }}
+            >
+              {day}
+            </div>
+          ))}
+        </div>
+      </div>
     </aside>
   );
 };
--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import './reset.css';
+import './variables.css';
 
 ReactDOM.render(
   <App />,
