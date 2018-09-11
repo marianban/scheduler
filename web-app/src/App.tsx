e624bac71bf956b53a3481c0c2e4e17ec701f2c7
Marian Ban <marian.ban@gmail.com>
2018-09-11 20:54:51 +0200

    week schedule

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button } from 'src/components/Button';
 import './App.css';
 import LeftPane from './calendar/left-pane/LeftPane';
+import Schedule from './calendar/schedule/Schedule';
 
 class App extends React.Component {
   public render() {
@@ -9,18 +10,29 @@ class App extends React.Component {
       <div className="app">
         <header className="app__header">
           <nav className="header__nav">
-            <a href="" className="header__nav__calendar header__nav__item--active">Calendar</a>
-            <a href="" className="header__nav__clients">Clients</a>
-            <a href="" className="header__nav__staff">Staff</a>
+            <a
+              href=""
+              className="header__nav__calendar header__nav__item--active"
+            >
+              Calendar
+            </a>
+            <a href="" className="header__nav__clients">
+              Clients
+            </a>
+            <a href="" className="header__nav__staff">
+              Staff
+            </a>
           </nav>
         </header>
         <LeftPane />
         <aside className="app__right-pane" />
         <main className="app__main-pane">
-          <Button>Today</Button>
-          <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div className="app__main-pane__controls">
+            <Button>Today</Button>
+          </div>
+          <div className="app__main-pane__content">
+            <Schedule />
+          </div>
         </main>
       </div>
     );
---
+++ web-app/src/calendar/schedule/Schedule.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import * as addMinutes from 'date-fns/addMinutes';
+import * as format from 'date-fns/format';
+import * as React from 'react';
+import './schedule.css';
+
+const Schedule = () => {
+  const now = new Date()
+  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0)
+  return (
+    <div className="schedule__week">
+      {Array.from({ length: 16 * 8 }).map((v, i) => (
+        <div
+          key={i}
+          className={classNames({
+            'schedule__week__day--last': (i + 1) % 8 === 0,
+            schedule__week__hour: (i + 1) % 8 === 1
+          })}
+          style={{
+            gridColumn: (i + 1) % 8,
+            gridRow: Math.ceil((i + 1) / 8)
+          }}
+        >
+         { (i + 1) % 8 === 1 && <span>{format(addMinutes(startOfDay, 30 * Math.ceil((i + 1) / 8)), 'H:mm')}</span> }
+        </div>
+      ))}
+      <div className="apointment">Appointment</div>
+    </div>
+  );
+};
+
+export default Schedule;
