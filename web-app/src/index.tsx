b69ac765f007e30b980afbab811bbbf690919147
Marian Ban <marian.ban@gmail.com>
2018-10-04 21:35:21 +0200

    mobx use strict mode

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,12 +1,17 @@
+import { configure } from 'mobx';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 // tslint:disable-next-line:ordered-imports 
-import './reset.css';
-// tslint:disable-next-line:ordered-imports 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+// tslint:disable-next-line:ordered-imports 
+import './reset.css';
 import './variables.css';
+
+configure({
+  enforceActions: true,
+});
 
 ReactDOM.render(
   <App />,
